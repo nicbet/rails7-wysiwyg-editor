00cd6acdfcc6b5f4b9c99d2dc4792b8e649b21d3
Nicolas Bettenburg <nicbet@kuy.io>
2021-10-08 19:56:52 -0400

    Adding Fragments

--- app/controllers/fragments_controller.rb
+++ app/controllers/fragments_controller.rb
@@ -1,6 +1,14 @@
 class FragmentsController < ApplicationController
   before_action :set_fragment, only: [:update]
 
+  def create
+    @fragment = Fragment.new(fragment_params)
+    if @fragment.valid?
+      @fragment.save
+    end
+    redirect_to(root_path)
+  end
+
   def update
     @fragment.update(data: params[:data])
     render @fragment
@@ -10,5 +18,9 @@ class FragmentsController < ApplicationController
 
   def set_fragment
     @fragment = Fragment.find(params[:id])
+  end
+
+  def fragment_params
+    params.require(:fragment).permit(:element, :data, :meta, :position)
   end
 end
